1c9659581c9f5bfbc121086de64013e00e2baae7
lenkon <inoulenkon@gmail.com>
2023-05-02 18:40:27 +0530

    Add game and author classes

---
+++ classes/game.rb
@@ -0,0 +1,15 @@
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at)
+    super()
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && last_played_at < 2.years.ago
+  end
+end
