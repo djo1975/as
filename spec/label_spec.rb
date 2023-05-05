6f205960d44fe2858b7b118ba2339f8663ac0cbb
otmanTR <r.brk.otman@gmail.com>
2023-05-05 13:31:18 +0300

    Update unit tests

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,14 +1,24 @@
 require 'rspec'
 require_relative '../classes/label'
+require_relative '../classes/item'
 
 describe Label do
   let(:color) { 'white' }
   let(:title) { 'Penguin' }
   let(:label) { Label.new(title, color) }
+
   describe '#initialize' do
     it 'sets color and title' do
       expect(label.color).to eq('white')
       expect(label.title).to eq('Penguin')
+    end
+  end
+  describe '#add_item' do
+    it 'adds an item to the label' do
+      item = Item.new('NewItem')
+      label.add_item(item)
+      expect(label.items).to include(item)
+      expect(item.label).to eq(label)
     end
   end
 end
