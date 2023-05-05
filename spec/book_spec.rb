e69423ccb7323f589802af1c8be96b58cdadf3d6
otmanTR <r.brk.otman@gmail.com>
2023-05-05 13:03:12 +0300

    Update tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,36 +1,29 @@
-# require 'rspec'
-# require_relative '../classes/book'
+require 'rspec'
+require_relative '../classes/book'
 
-# describe Book do
-#   let(:publisher) { 'Penguin' }
-#   let(:cover_state) { 'Good' }
-#   let(:publish_date) { Date.today - 1.year }
-#   let(:book) { Book.new('Book Name', publisher, cover_state, publish_date) }
+describe Book do
+  let(:publisher) { 'Penguin' }
+  let(:cover_state) { 'bad' }
+  let(:publish_date) { Date.today - 12.year }
+  let(:book) { Book.new('Book Name', publisher, publish_date, cover_state) }
 
-#   describe '#initialize' do
-#     it 'sets publisher and cover_state' do
-#       expect(book.publisher).to eq(publisher)
-#       expect(book.cover_state).to eq(cover_state)
-#     end
-#   end
+  describe '#initialize' do
+    it 'sets publisher and cover_state' do
+      expect(book.publisher).to eq('Penguin')
+      expect(book.cover_state).to eq('bad')
+    end
+  end
 
-#   describe '#can_be_archived?' do
-#     context 'when cover state is not bad' do
-#       it 'returns true' do
-#         expect(book.can_be_archived?).to be_truthy
-#       end
-#     end
+  describe '#can_be_archived?' do
+    context 'when cover state is bad' do
+      it 'returns true' do
+        expect(book.can_be_archived?).to be_truthy
+      end
+    end
 
-#     context 'when cover state is bad' do
-#       let(:cover_state) { 'Bad' }
-#       it 'returns true if publish date is more than 5 years ago' do
-#         expect(book.can_be_archived?).to be_truthy
-#       end
-
-#       it 'returns false if publish date is less than or equal to 5 years ago' do
-#         book.publish_date = Date.today - 5.years
-#         expect(book.can_be_archived?).to be_falsey
-#       end
-#     end
-#   end
-# end
+      it 'returns true if publish date is less than or equal to 10 years ago' do
+        book.publish_date < Date.today - 10.years
+        expect(book.can_be_archived?).to be_truthy
+      end
+    end
+end
